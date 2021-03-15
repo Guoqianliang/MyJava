9af3525e408d0d92dc7e9c5a0969243de2380af8
gql <guoqianliang@foxmail.com>
2021-03-15 23:24:46 +0800

    2021.3.15

--- Exercise/src/com/gql/test10/Demo01.java
+++ Exercise/src/com/gql/test10/Demo01.java
@@ -4,7 +4,7 @@ package com.gql.test10;
  * @Description: 计算从今天起，100天以后是几月几号，并格式化成xxxx年x月x日的形式打印出来。
  * 提示：
  * （1）调用Calendar类的add()方法计算100天后的日期
- * （2）调用Calendar类的getTime()方法返回Date类型对象
+ * （2）调用Calendar类的getTime()方法返回Date类型对象.
  * （3）SimpleDateFormat对象格式化日期
  * @author Guoqianliang
  * @date 23:10 - 2021/3/15
