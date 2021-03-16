338bb4f1301a8377a0a11a7345b6c427825a0653
gql <guoqianliang@foxmail.com>
2021-03-16 10:54:58 +0800

    2021.3.16

--- Exercise/src/com/gql/test10/Demo02.java
+++ Exercise/src/com/gql/test10/Demo02.java
@@ -7,7 +7,7 @@ import java.util.Iterator;
  * @Description: 使用ArrayList集合，对其中添加10个不同的元素，并使用Iterator遍历集合。
  * 提示：
  *  （1）使用add()方法将元素添加到ArrayList集合中
- *  （2）调用集合iterator()方法获得Iterator对象，并调用Iterator的hasNext()和next()方法，迭代出集合的所有元素
+ *  （2）调用集合iterator()方法获得Iterator对象，并调用Iterator的hasNext()和next()方法，迭代出集合的所有元素。
  * @author Guoqianliang
  * @date 10:45 - 2021/3/16
  */
