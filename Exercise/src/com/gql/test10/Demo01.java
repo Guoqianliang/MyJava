174dfefdc7e4b362589f467af4b238e62f925eb5
gql <guoqianliang@foxmail.com>
2021-03-16 10:53:56 +0800

    2021.3.16

--- Exercise/src/com/gql/test10/Demo01.java
+++ Exercise/src/com/gql/test10/Demo01.java
@@ -1,5 +1,10 @@
 package com.gql.test10;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Scanner;
+
 /**
  * @Description: 计算从今天起，100天以后是几月几号，并格式化成xxxx年x月x日的形式打印出来。
  * 提示：
@@ -11,7 +16,34 @@ package com.gql.test10;
  */
 public class Demo01 {
     public static void main(String[] args) {
-        System.out.println("123");
-        System.out.println("1");
+        Scanner in = new Scanner(System.in);
+        System.out.println("本程序可以计算x天之后的日期,请输入x:");
+        int x = in.nextInt();
+        System.out.println(x + "天之后的日期是:" + Util.calculateDate(x));
+    }
+}
+
+/**
+ * 工具类
+ */
+class Util {
+    /**
+     * 计算x天之后的日期
+     * 关于Calendar类:
+     *   .getInstance(): 创建Calendar对象
+     *   .add(int field, int amount): 为calendar对象增加amount天
+     *   .getTime(): 获得Data对象
+     *
+     * @param x : 输入的天数
+     * @return String : xxxx年x月x日
+     */
+    public static String calculateDate(int x) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.DATE, x);
+        Date data = calendar.getTime();
+
+        // 日期格式化
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy年MM月dd日");
+        return sdf.format(data);
     }
 }
---
+++ Exercise/src/com/gql/test10/Demo02.java
@@ -0,0 +1,27 @@
+package com.gql.test10;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+/**
+ * @Description: 使用ArrayList集合，对其中添加10个不同的元素，并使用Iterator遍历集合。
+ * 提示：
+ *  （1）使用add()方法将元素添加到ArrayList集合中
+ *  （2）调用集合iterator()方法获得Iterator对象，并调用Iterator的hasNext()和next()方法，迭代出集合的所有元素
+ * @author Guoqianliang
+ * @date 10:45 - 2021/3/16
+ */
+public class Demo02 {
+    public static void main(String[] args) {
+        ArrayList list = new ArrayList();
+        int var = 10;
+        for (int i = 1; i <= var; i++) {
+            list.add(i);
+        }
+        Iterator it = list.iterator();
+        while (it.hasNext()) {
+            Object obj = it.next();
+            System.out.println(obj);
+        }
+    }
+}
