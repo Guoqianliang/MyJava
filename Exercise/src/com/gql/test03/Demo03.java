bacc4439b9ff13f62dc7060d19767c16cb43670d
gql <guoqianliang@foxmail.com>
2021-06-20 17:45:57 +0800

    2021.6.20 Version_last

--- Exercise/src/com/gql/test03/Demo03.java
+++ Exercise/src/com/gql/test03/Demo03.java
@@ -9,7 +9,7 @@ public class Demo03 {
         // 外循环每次都输出1行,共输出9行
         for (int i = 1; i <= 9; i++) {
             for (int j = 1; j <= i; j++) {
-                System.out.print(i + "*" + j + "=" + i * j + "\t");
+                System.out.print(j + "*" + i + "=" + i * j + "\t");
             }
             System.out.println();
         }
